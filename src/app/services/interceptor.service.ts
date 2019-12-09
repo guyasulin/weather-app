7f9646c72c453ea5a73f49b1c500d4d104804c94
unknown <guy@justmanage.com>
2019-12-09 11:00:59 +0200

    change intceptor

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,24 +1,29 @@
 import { Injectable, Injector } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpHeaders } from '@angular/common/http';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 import { environment } from 'src/environments/environment';
+// import {NotifService} from '../services/notification/notification.service';
 
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
     constructor(private injector: Injector) { }
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
         if (req.url.indexOf('http') != 0) {
-            if (environment.hasOwnProperty('headers') && environment) {
+            if (environment.hasOwnProperty('apiBaseUrl') && environment.production) {
+                req = req.clone({ url: environment.production + req.url });
+            }
+
+            if (environment.hasOwnProperty('headers') && environment.production) {
                 for (const prop in environment) {
-                    if (environment.hasOwnProperty(prop)) {
-                        req = req.clone({ headers: req.headers.set(prop, environment[prop]) });
+                    if (environment.production.hasOwnProperty(prop)) {
+                        req = req.clone({ headers: req.headers.set(prop, environment.production[prop]) });
                     }
                 }
             }
         }
 
-        return next.handle(req);
+        // return next.handle(req).catch(this.catchErrors());
+        return next.handle(req);//.catch(this.catchErrors());
 
     }
- }
+}
